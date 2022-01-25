2bb6fecc5be5a5400599c32f427e2c4aa5304644
Telma Rosário <telmarrosario@gmail.com>
2022-01-25 15:31:03 +0000

    Add react-icons library, build icons for the sidebar and change layer for custom css class

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,11 +1,14 @@
+import SideBarIcons from "./SideBarIcons";
+import { BsPlus, BsFillLightningFill, BsGearFill } from "react-icons/bs";
+import { FaFire, FaPoo } from "react-icons/fa";
+
 function SideBar() {
   return (
     <div className="fixed top-0 left-0 h-screen w-16 flex flex-col bg-gray-900 text-white shadow-lg">
-      <i>A</i>
-      <i>B</i>
-      <i>C</i>
-      <i>D</i>
-      <i>E</i>
+      <SideBarIcons icon={<FaFire size="28" />} />
+      <SideBarIcons icon={<BsPlus size="32" />} />
+      <SideBarIcons icon={<BsFillLightningFill size="20" />} />
+      <SideBarIcons icon={<FaPoo size="20" />} />
     </div>
   );
 }
